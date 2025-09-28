edaeed41b3c2cdcf1f2d3396e35aa0a72d39d56f
Courtney Macgregor <cjoss90@hotmail.com>
2025-09-28 11:37:56 +1000

    Done

---
+++ lib/migrations/versions/66cf97c95f27_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 66cf97c95f27
+Revises: 
+Create Date: 2025-09-28 11:08:45.270292
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '66cf97c95f27'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
